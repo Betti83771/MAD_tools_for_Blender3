c95268d01194bdd55bb83553b73cd4f1d5fc4035
Betti <akylu8@gmail.com>
2023-11-10 16:12:16 +0100

    added preference ui files

---
+++ MAD_Animation_Tools/preferences_ui.py
@@ -0,0 +1,83 @@
+import bpy
+from . import addon_updater_ops, addon_updater_ops_global
+
+@addon_updater_ops.make_annotations
+class Prefs(bpy.types.AddonPreferences):
+    bl_idname = __package__
+
+    # addon updater preferences from `__init__`, be sure to copy all of them
+    auto_check_update = bpy.props.BoolProperty(
+        name = "Auto-check for Update",
+        description = "If enabled, auto-check for updates using an interval",
+        default = False,
+    )
+
+    updater_interval_months = bpy.props.IntProperty(
+        name='Months',
+        description = "Number of months between checking for updates",
+        default=0,
+        min=0
+    )
+    updater_interval_days = bpy.props.IntProperty(
+        name='Days',
+        description = "Number of days between checking for updates",
+        default=7,
+        min=0,
+    )
+    updater_interval_hours = bpy.props.IntProperty(
+        name='Hours',
+        description = "Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23
+    )
+    updater_interval_minutes = bpy.props.IntProperty(
+        name='Minutes',
+        description = "Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59
+    )
+
+    auto_check_update_global = bpy.props.BoolProperty(
+        name = "Auto-check for Update",
+        description = "If enabled, auto-check for updates using an interval",
+        default = False,
+    )
+
+    updater_interval_months_global = bpy.props.IntProperty(
+        name='Months',
+        description = "Number of months between checking for updates",
+        default=0,
+        min=0
+    )
+    updater_interval_days_global = bpy.props.IntProperty(
+        name='Days',
+        description = "Number of days between checking for updates",
+        default=7,
+        min=0,
+    )
+    updater_interval_hours_global = bpy.props.IntProperty(
+        name='Hours',
+        description = "Number of hours between checking for updates",
+        default=0,
+        min=0,
+        max=23
+    )
+    updater_interval_minutes_global = bpy.props.IntProperty(
+        name='Minutes',
+        description = "Number of minutes between checking for updates",
+        default=0,
+        min=0,
+        max=59
+    )
+    
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text=f"Update {__package__} addon")
+        addon_updater_ops.update_settings_ui(self,context)
+        row = layout.row()
+        row.label(text=f"Update all the MAD Tools addons")
+        addon_updater_ops_global.update_settings_ui_condensed(self,context)
